7bfb0b2dafe112597eabb27dcaccf2096629ffc4
wellington <wellington.h.s.oliveira@hotmail.com>
2024-04-02 22:18:39 -0300

    fiz umas melhoras

--- pergunta1/Condições.js
+++ pergunta1/Condições.js
@@ -1,5 +1,7 @@
 
 let indice = 13, soma = 0, k = 0;
+/* laços de repetição até chegar no valor do indice e dps gerar a conta exata 
+o valor foi de 91  */
 
 while(k < indice){
     k = k + 1;
--- pergunta5/Inverter.js
+++ pergunta5/Inverter.js
@@ -6,10 +6,10 @@ function inverterString(converter) {
     return novaString;
 }
 
-// como fazer para usar 
-// não consegui pensar em uma forma de deixar a instring como maravilhoso e Deus
+// como fazer para usar: é so colocar o valor na stringAtual
+// da para usar em forma de array também [5,4,3,2,1] porem achei assim melhor "5,4,3,2,1"
 
-let StringAtual = 'Deus é maravilhoso';
+let StringAtual = "5,4,3,2,1";
 
 let stringInvertida = inverterString(StringAtual);
 
