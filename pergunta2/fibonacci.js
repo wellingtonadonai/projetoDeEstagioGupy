6fd1e8dc62a063bff05eaa1381a8b7e642522f44
wellington <wellington.h.s.oliveira@hotmail.com>
2024-04-03 00:07:56 -0300

    mudei algumas coisas no html

--- pergunta2/fibonacci.js
+++ pergunta2/fibonacci.js
@@ -14,10 +14,14 @@ function verificaFibonacci(numero) {
 
 let numeroInformado = parseInt (prompt ("Digite um número para verificar se pertence à sequência de Fibonacci: "));
 
+//deixei comentado o console caso deseje executar no console do navegador ou em outro console 
+// No caso e so comentar o alert por boas praticas
 
 if (verificaFibonacci(numeroInformado)) {
+    //console.log(`O número ${numeroInformado} pertence à sequência de Fibonacci.`);
     alert(`O número ${numeroInformado} pertence à sequência de Fibonacci.`);
 } else {
+    //console.log(`O número ${numeroInformado} não pertence à sequência de Fibonacci.`);
     alert(`O número ${numeroInformado} não pertence à sequência de Fibonacci.`);
 }
 
