35f9b1c59ed1e8f22781dc13f8da1c0eb3f985ce
wellington <wellington.h.s.oliveira@hotmail.com>
2024-04-02 22:30:09 -0300

    fiz um comentario em condições

--- pergunta1/Condições.js
+++ pergunta1/Condições.js
@@ -1,6 +1,6 @@
 
 let indice = 13, soma = 0, k = 0;
-/* laços de repetição até chegar no valor do indice e dps gerar a conta exata 
+/* laços de repetição até chegar no valor do indice e depois gerar a conta exata 
 o valor foi de 91  */
 
 while(k < indice){
